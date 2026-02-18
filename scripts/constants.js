bb80d4610244049dd97ecbb9cddd45dc789096b0
fe_fi <fe_fi@gmx.net>
2026-02-18 08:46:17 +0100

    v13 compatibility

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,7 +1,4 @@
 export const MODULE = "boneyard-drawing-tools";
-export const SETTING_SHORTCUT_ENABLED = "shortcut_enabled";
-export const SETTING_SHORTCUT_KEY = "shortcut_key";
-export const SETTING_SHORTCUT_MODIFIERS = "shortcut_modifiers";
 export const SETTINGS_COLOR_SELECTOR_SIZE = "color_selector_size";
 export const SETTING_DROPPER_PREVIEW_SIZE = "dropper_preview_size";
 export const SETTING_DROPPER_READ_RADIUS = "dropper_read_radius";
